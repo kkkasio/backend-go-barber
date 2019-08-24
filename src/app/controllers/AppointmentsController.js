5fa70e86aaed84c6398c87eea7dbf37c35458f65
Kásio Eduardo <kasioeduardo13@gmail.com>
2019-08-24 20:57:20 -0300

    feat: Queue send email on appointment cancellation

--- src/app/controllers/AppointmentsController.js
+++ src/app/controllers/AppointmentsController.js
@@ -4,10 +4,10 @@ import pt from 'date-fns/locale/pt';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/Files';
-
-import mail from '../../lib/Mail';
-
 import notification from '../schemas/Notification';
+
+import Queue from '../../lib/Queue';
+import CancellationMail from '../jobs/CancellationMail';
 
 class AppointmentController {
   async index(req, res) {
@@ -159,18 +159,10 @@ class AppointmentController {
 
     await appointment.save();
 
-    await mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento Cancelado',
-      template: 'cancelation',
-      context: {
-        provider: appointment.provider.name,
-        user: appointment.user.name,
-        date: format(appointment.date, "'dia' dd 'de' MMMM', ás' H:mm'h'", {
-          locale: pt,
-        }),
-      },
+    await Queue.add(CancellationMail.key, {
+      appointment,
     });
+
     return res.json(appointment);
   }
 }
